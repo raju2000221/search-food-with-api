9c72d4fdaecd163c14664499c784a75e330a1039
raju2000221 <bdrajuislam246@gmail.com>
2023-03-28 00:44:57 +0600

    add modal

--- meal.js
+++ meal.js
@@ -20,11 +20,12 @@ const mealDiv = document.createElement('div');
             <div class="card-body">
             <h5 class="card-title">${meal.strMeal}</h5>
             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetails">
+                Launch static backdrop modal
+                </button>
+
             </div>
-            <div class="card-footer">
-            <small class="text-body-secondary">Last updated 3 mins ago</small>
             </div>
-        </div>
         `
         melasContainerr.appendChild(mealDiv);
     });
