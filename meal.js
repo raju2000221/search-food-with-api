bb9555c8f9f252fc4916632414432dd90d55e336
raju2000221 <bdrajuislam246@gmail.com>
2023-03-28 01:05:53 +0600

    get image in modal by id

--- meal.js
+++ meal.js
@@ -20,8 +20,8 @@ const mealDiv = document.createElement('div');
             <div class="card-body">
             <h5 class="card-title">${meal.strMeal}</h5>
             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetails">
-                Launch static backdrop modal
+            <button onclick="loadMealDetails(${meal.idMeal})" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetails">
+                Details
                 </button>
 
             </div>
@@ -36,5 +36,21 @@ const searchMeal = () =>{
    loadMeals(serchText)
 //    console.log(serchText)
 
+}
+
+const loadMealDetails = idmeal =>{
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idmeal}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data=> displayMealDetails(data.meals[0]))
+    console.log(idmeal)
+}
+
+const displayMealDetails = meal =>{
+    document.getElementById('mealDetailsLabel').innerText = meal.strMeal;
+    const mealDetails = document.getElementById('mealdetailsBody');
+    mealDetails.innerHTML=`
+    <img class = img-fluid src="${meal.strMealThumb}">
+    `
 }
 loadMeals('fish');
