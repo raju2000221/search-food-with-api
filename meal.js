bdaa37dcdadba18c754f654dbd1614bb299bdf50
raju2000221 <bdrajuislam246@gmail.com>
2023-03-27 23:02:42 +0600

    add button to search

---
+++ meal.js
@@ -0,0 +1,31 @@
+const loadMeals = () =>{
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=meat')
+    .then(res => res.json())
+    .then(data =>displayMeals(data.meals))
+
+}
+
+const displayMeals = meals =>{
+    // console.log(meals)
+    const melasContainerr = document.getElementById('melasContainer');
+    meals.forEach(meal => {
+        console.log(meal)
+const mealDiv = document.createElement('div');
+        mealDiv.classList.add('col');
+        mealDiv.innerHTML =`
+        <div class="card h-100">
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${meal.strMeal}</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            <div class="card-footer">
+            <small class="text-body-secondary">Last updated 3 mins ago</small>
+            </div>
+        </div>
+        `
+        melasContainerr.appendChild(mealDiv);
+    });
+
+}
+loadMeals();
