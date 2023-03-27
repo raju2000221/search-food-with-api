5d5f5d08a3bd72a8595d0021bc5dd6e1eade9cb7
raju2000221 <bdrajuislam246@gmail.com>
2023-03-28 01:42:55 +0600

    add details on modal

--- meal.js
+++ meal.js
@@ -19,7 +19,8 @@ const mealDiv = document.createElement('div');
             <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
             <h5 class="card-title">${meal.strMeal}</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class= mt-5 >  Area : ${meal.strArea} Food</p>
+             <p class= mt-2 >  Category : ${meal.strCategory}</p>
             <button onclick="loadMealDetails(${meal.idMeal})" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetails">
                 Details
                 </button>
@@ -51,6 +52,12 @@ const displayMealDetails = meal =>{
     const mealDetails = document.getElementById('mealdetailsBody');
     mealDetails.innerHTML=`
     <img class = img-fluid src="${meal.strMealThumb}">
+
+    <p class= mt-5 >  Area : ${meal.strArea}</p>
+    <p class= mt-2 >  Category : ${meal.strCategory}</p>
+    <p class= mt-2 >  Ingredient : ${meal.strIngredient1+' , '+meal.strIngredient2+' , '+meal.strIngredient3+' , '+meal.strIngredient4+' , '+meal.strIngredient5+' , '+meal.strIngredient6+' , '+meal.strIngredient7+' , '+meal.strIngredient8+' , '+meal.strIngredient9+' , '+meal.strIngredient10+' , '+meal.strIngredient11+' , '+meal.strIngredient12+' , '+meal.strIngredient13}</p>
+    <a href=" ${meal.strYoutube}"><button  class= "btn btn-info">Watch Video </button></a>
+   
     `
 }
 loadMeals('fish');
